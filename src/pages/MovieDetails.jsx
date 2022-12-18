ba3687e31af46e7960863a08fb6713967df93bcb
kartikkpawar <knpwar789@gmail.com>
2022-12-18 12:34:33 +0530

    Movie details page responsive

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -34,7 +34,7 @@ const MovieDetails = () => {
   const recommendationMessage = (vote) => {
     if (vote * 10 > 70) {
       return (
-        <span className="text-white text-md font-medium">
+        <span className="text-white text-sm xl:text-md font-medium">
           Our Users Are Recommending It
         </span>
       );
@@ -160,31 +160,33 @@ const MovieDetails = () => {
     !isDetailsLoading && (
       <div className="bg-black-background h-screen w-full overflow-scroll hideScrollBar">
         <div
-          className="h-4/6 rounded-bl-3xl rounded-br-3xl p-8 bg-cover bg-center"
+          className="aspect-video w-full xl:h-4/6 rounded-bl-3xl rounded-br-3xl p-8 xl:bg-cover xl:bg-center"
           style={{
             background: `linear-gradient(180deg, rgba(255,255,255,0.50) 0%, rgba(212,212,220,0) 0%, rgba(0,0,0,0.8) 91%), url(https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${details.backdrop_path})`,
           }}
         >
           <div className="flex items-center">
             <div
-              className="p-3 bg-white bg-opacity-60 text-black rounded-full mr-6 w-max"
+              className="p-1 xl:p-3 bg-white bg-opacity-60 text-black rounded-full mr-4 xl:mr-6 w-max"
               onClick={() => navigate("/")}
             >
               <IoMdArrowBack className="cursor-pointer text-2xl" />
             </div>
             <div className="flex items-center justify-between w-full">
-              <span className="text-white text-2xl font-medium">
+              <span className="text-white text-sm xl:text-2xl font-medium">
                 {details.title}
               </span>
-              <div className=" py-2 bg-white bg-opacity-30 text-black rounded-full mr-6 w-max px-5">
-                <span className="text-white text-xl font-medium">Movies</span>
+              <div className=" py-2 bg-white bg-opacity-30 text-black rounded-full mr-6 w-max xl:px-5 px-4">
+                <span className="text-white text-sm xl:text-xl font-medium">
+                  Movies
+                </span>
               </div>
             </div>
           </div>
           <div className="h-full flex items-end pb-8 ">
             <div className="w-7/12 flex justify-between items-center">
               <div className="flex items-center">
-                <div className="h-20 w-20">
+                <div className="xl:h-20 xl:w-20 h-12 w-12">
                   <CircularProgressbarWithChildren
                     value={details.vote_average * 10}
                     background={false}
@@ -193,23 +195,23 @@ const MovieDetails = () => {
                       pathColor: "#e21717",
                     })}
                   >
-                    <span className="text-white font-bold text-xl">
-                      {details.vote_average}
+                    <span className="text-white font-bold text-[8px] xl:text-xl">
+                      {Math.round(details.vote_average * 10)}%
                     </span>
                   </CircularProgressbarWithChildren>
                 </div>
                 <div className="ml-3 flex flex-col">
-                  <span className="text-white text-lg font-semibold uppercase">
+                  <span className="text-white text-sm xl:text-lg font-semibold uppercase">
                     {SimplifyNumber(details.vote_count)} Votes
                   </span>
                   {recommendationMessage(details.vote_average)}
                 </div>
               </div>
               <div
-                className=" group hover:bg-white flex items-center border rounded-full border-white w-52 h-12 justify-center px-10 text-proj-red cursor-pointer select-none text-xl"
+                className=" group hover:bg-white flex items-center border rounded-full border-white w-36 xl:w-52 h-8 xl:h-12 justify-center px-10 text-proj-red cursor-pointer select-none text-sm xl:text-xl"
                 onClick={() => setModalOpen(true)}
               >
-                <BsFillPlayFill className="text-2xl" />
+                <BsFillPlayFill className="text-sm xl:text-2xl" />
                 <span className="ml-3 text-white font-semibold group-hover:text-proj-red">
                   Trailer
                 </span>
@@ -217,28 +219,32 @@ const MovieDetails = () => {
             </div>
           </div>
         </div>
-        <div className=" mt-12 h-full bg-black-background flex px-8">
-          <div className="w-1/5 flex flex-col h-full items-center">
-            <div className="h-96 mr-3">
+        <div className=" mt-12 h-full bg-black-background flex px-8  flex-col xl:flex-row">
+          <div className="w-full xl:w-1/5 flex flex-col h-full items-center">
+            <div className="h-72 xl:h-96 mr-3">
               <img
                 src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${details.poster_path}`}
                 alt=""
                 className="h-full rounded-md"
               />
             </div>
-            <div className="w-72 mt-8 flex flex-col">
-              <span className="text-white text-4xl">
+            <div className="w-72 mt-8 flex xl:flex-col items-center xl:items-start justify-center xl:justify-start gap-3">
+              <span className="text-white text-2xl xl:text-4xl">
                 {moment(details.release_date).format("YYYY")}
               </span>{" "}
-              <span className="text-white text-4xl mt-1 uppercase">
+              <span className="text-white text-2xl xl:text-4xl xl:mt-1 uppercase">
                 {shortEnglishHumanizer(details.runtime * 1000 * 60)}
               </span>
             </div>
-            <div className="w-72 mt-8">
-              <span className="text-white text-4xl mt-1 uppercase font-thin">
-                Available On
-              </span>
-              {!isWatchProvidersLoading && (
+
+            {!isWatchProvidersLoading && (
+              <div className="w-72 mt-8">
+                {watchProviders?.results["US"]?.buy?.length > 0 && (
+                  <span className="text-white text-4xl mt-1 uppercase font-thin">
+                    Available On
+                  </span>
+                )}
+
                 <div className="flex mt-3 flex-wrap">
                   {watchProviders?.results["US"]?.buy?.map((provider) => (
                     <img
@@ -248,30 +254,30 @@ const MovieDetails = () => {
                     />
                   ))}
                 </div>
-              )}
-            </div>
+              </div>
+            )}
           </div>
-          <div className="w-3/5 px-5">
-            <span className="text-white text-4xl font-semibold">
+          <div className="w-full xl:w-3/5 px-5">
+            <span className="text-white text-2xl xl:text-4xl font-semibold">
               {details.title}
             </span>
             <div className="flex mt-5 flex-wrap">
               {details.genres.map((genre) => (
-                <div className="flex items-center border rounded-full mb-3  border-red-300 w-40 px-3 h-10 justify-center text-white select-none text-md mr-3 font-semibold">
+                <div className="flex items-center border rounded-full mb-3  border-red-300 xl:w-40 max:w-36 px-3 h-7 xl:h-10 justify-center text-white select-none text-xs xl:text-md mr-3 font-semibold">
                   <span className="text-red-300">{genre.name}</span>
                 </div>
               ))}
             </div>
             <div className="mt-5 flex items-center">
               <div
-                className="group flex items-center rounded-full  w-52 h-16 justify-center px-10 text-white hover:text-proj-red hover:bg-white cursor-pointer select-none text-2xl bg-proj-red"
+                className="group flex items-center rounded-full  w-36 h-10 xl:w-52 xl:h-16 justify-center px-4 xl:px-10 text-white hover:text-proj-red hover:bg-white cursor-pointer select-none text-base xl:text-2xl bg-proj-red"
                 onClick={() => setModalOpen(true)}
               >
-                <BsFillPlayFill className="text-2xl" />
+                <BsFillPlayFill className="text-base xl:text-2xl" />
                 <span className="ml-3 font-semibold">Watch</span>
               </div>
               <div
-                className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-4 justify-center text-white select-none text-2xl mx-3 font-semibold"
+                className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-2 xl:p-4 justify-center text-white select-none text-xs xl:text-2xl mx-3 font-semibold"
                 onClick={() => {
                   handleFavourites(
                     details.id,
@@ -285,7 +291,7 @@ const MovieDetails = () => {
                   <AiTwotoneHeart />
                 </span>
               </div>{" "}
-              <div className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-4 justify-center text-white select-none text-2xl mx-3 font-semibold">
+              <div className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-2 xl:p-4 justify-center text-white select-none text-xs xl:text-2xl mx-3 font-semibold">
                 <span className="text-red-600 group-hover:text-white">
                   <CopyToClipboard
                     text={`${appUrl}${location.pathname}`}
@@ -296,7 +302,7 @@ const MovieDetails = () => {
                 </span>
               </div>{" "}
               <div
-                className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-4 justify-center text-white select-none text-2xl mx-3 font-semibold"
+                className="group flex items-center border-2 rounded-full border-gray-300 hover:border-red-600 w-max p-2 xl:p-4 justify-center text-white select-none text-xs xl:text-2xl mx-3 font-semibold"
                 onClick={() =>
                   handleWatchlist(
                     details.id,
@@ -328,18 +334,18 @@ const MovieDetails = () => {
                   {cast.cast.map(
                     (mem) =>
                       mem.profile_path && (
-                        <div className="flex items-center ml-6 mb-5 w-1/3">
+                        <div className="flex items-center xl:ml-6 mb-5 xl:w-1/3">
                           <img
                             src={`https://image.tmdb.org/t/p/original${mem.profile_path}`}
                             alt={mem.name}
-                            className="h-28 rounded-md "
+                            className="h-20 xl:h-28 rounded-md "
                           />
                           <div className="ml-4">
-                            <span className="text-white text-xl font-md">
+                            <span className="text-white text-xs xl:text-xl font-md">
                               {mem.character}
                             </span>
                             <br />
-                            <span className="text-white text-xl font-thin">
+                            <span className="text-white text-xs xl:text-xl font-thin">
                               {mem.name}
                             </span>
                           </div>
@@ -350,9 +356,11 @@ const MovieDetails = () => {
               )}
             </div>
           </div>
-          <div className="w-1/5">
+          <div className="w-full xl:w-1/5">
             {" "}
-            <span className="mt-8 text-xl font-normal">More Like This</span>
+            <span className="mt-8 text-xl font-normal text-white">
+              More Like This
+            </span>
             {!isSimilarMediaLoading && (
               <div className="mt-5 flex flex-wrap justify-center h-2/4 hideScrollBar">
                 {similarMedia.results.map(
